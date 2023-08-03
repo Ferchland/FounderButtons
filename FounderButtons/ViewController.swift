14d70a6a1e6382f2f874e17834c315a92ca6b327
Andrew Ferchland <aferchland@me.com>
2023-08-03 11:17:26 -0400

    Ch 2.3 Exercise 3

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var founderOne: UILabel!
+    @IBOutlet weak var founderTwo: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func googleFounderButtonPressed(_ sender: UIButton) {
+        founderOne.text = "Larry Page"
+        founderOne.textColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+        founderTwo.text = "Sergey Brin"
+        founderTwo.textColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+    }
+    
 
-
+    @IBAction func runwayFounderButtonPressed(_ sender: UIButton) {
+        founderOne.text = "Jennifer Hyman"
+        founderTwo.text = "Jenny Fleiss"
+        founderOne.textColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+        founderTwo.textColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+    }
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        founderOne.text = " "
+        founderTwo.text = " "
+    }
+    
 }
 
